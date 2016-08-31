30322926fe6b072871347bf129eee54f6da1927d
pam <pamela.garza.sny@gmail.com>
2016-08-31 14:54:26 -0500

    add button that allows user to store picture description

--- app.js
+++ app.js
@@ -11,45 +11,47 @@ app.set("view engine", "ejs");
 
 var myfeedSchema = new mongoose.Schema({
   name: String,
-  Age: Number,
+  description: String,
+  Age: String,
   image: String,
 });
 
 var Myfeed = mongoose.model("Myfeed", myfeedSchema);
 
-Myfeed.create(
-  {
-    name: "bethanny",
-    age: 25 ,
-    image: "https://farm6.staticflickr.com/5003/5347012547_617da5c271.jpg"
-
-  }, function(err, userUpload){
-    if(err){
-      console.log(err);
-    } else {
-        console.log("you made a new post");
-        console.log(userUpload);
-      }
-    });
+// Myfeed.create(
+//   {
+//     name: "bethanny",
+//     description: "This outfit is supper cute. you can literally wear anywhere!",
+//     age: "25",
+//     image: "https://farm6.staticflickr.com/5003/5347012547_617da5c271.jpg",
+//
+//   }, function(err, userUpload){
+//     if(err){
+//       console.log(err);
+//     } else {
+//         console.log("you made a new post");
+//         console.log(userUpload);
+//       }
+//     });
 
 
 app.get("/", function(req,res){
   res.render("homefeed");
 });
 
-
+//this is the index route!!!!!!!!!!!!!!!!!!!!!!! get a list of all the feeds.
 app.get("/myfeed", function(req,res){
 
   Myfeed.find({}, function(err, allmyfeeds){
     if(err){
       console.log(err);
     } else {
-      res.render("myfeed", {myfeed: allmyfeeds});
+      res.render("index", {myfeed: allmyfeeds});
     }
   });
 });
 
-
+//THIS IS THE CREATE ROUTEE!!!!!!!!
 app.post("/myfeed", function(req,res){
   var name = req.body.name;
   var image = req.body.image;
@@ -65,10 +67,30 @@ Myfeed.create(newMyfeed, function(err, newnewfeed){
 });
 
 
-
-app.get("/myfeed/new", function(req,res){
+//this is the NEW ROUTE!!!!!!!!!!!!!
+app.get("/myfeed/new", function(req, res){
   res.render("new.ejs");
 });
+
+
+
+
+// SHOW SHOWS MORE INFO ABOUT A PARTICUALR THING!!!!
+app.get("/myfeed/:id", function(req, res) {
+  //find the campground with provided ID
+  //this is a function that mongo give us:
+  Myfeed.findById(req.params.id, function(err, foundfeed){
+    if(err){
+      console.log(err);
+    } else {
+        //render/show template with that campground
+        res.render("details", {Myfeed: foundfeed});
+        }
+      });
+  })
+
+
+
 
 
 app.listen(3000, function(){
